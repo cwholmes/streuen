9fedb14a495eed9b6be528c349989cb758fd7270
cwholmes <cholmes5324@gmail.com>
2025-08-04 10:49:48 -0500

    Move mod definitions to lib.rs

---
+++ crates/chat-web/src/lib.rs
@@ -0,0 +1,4 @@
+#[cfg(target_arch = "wasm32")]
+pub mod app;
+#[cfg(target_arch = "wasm32")]
+mod chat;
--- crates/chat-web/src/main.rs
+++ crates/chat-web/src/main.rs
@@ -1,12 +1,7 @@
 #[cfg(target_arch = "wasm32")]
-mod app;
-#[cfg(target_arch = "wasm32")]
-mod chat;
-
-#[cfg(target_arch = "wasm32")]
 fn main() {
     tracing_wasm::set_as_global_default();
-    yew::Renderer::<app::App>::new().render();
+    yew::Renderer::<streuen_chat_web::app::App>::new().render();
 }
 
 #[cfg(not(target_arch = "wasm32"))]
